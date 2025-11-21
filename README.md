f9013b5541cf51d7f0cb2745820eb6586ad3129c
Rahul Mishra <rahul.mishra@mindstix.com>
2025-11-21 06:05:46 +0000

    added images in poc

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # Prop Hunt
 
+> A Ecommerce Store Consisting of Prop Used in Movies, that you can purchase as a reminder for your favorte piece of entermainment.
+
+
+
+
+
+
+
 
 
--- cartridges/app_custom_cat/cartridge/controllers/Default.js
+++ cartridges/app_custom_cat/cartridge/controllers/Default.js
@@ -2,37 +2,21 @@ var server = require('server');
 
 server.get('Start', (req, res, next) => {
     var ProductMgr = require('dw/catalog/ProductMgr');
-    //var product = ProductMgr.getProduct('008884303989');
+    var product = ProductMgr.getProduct('008884303989');
     var URLUtils = require('dw/web/URLUtils');
-    var ProductFactory = require('*/cartridge/scripts/factories/product');
-    var productTileParams = { pview: 'tile' };
-    var param = {
-        pview: 'tile',
-        pid: '008884303989'
-    };
-    var exp = null;
+    //var ProductFactory = require('*/cartridge/scripts/factories/product');
+    //var productTileParams = { pview: 'tile' };
 
-    // try{
-    //     var productTile = ProductFactory.get(param);
-    // }catch(e){
-    //     res.render('error', {
-    //         error: {
-                
-    //         },
-    //         showError: null,
-    //         message: 'Find'
-    //     })
-    //     exp = e;
-    //     return next();
-    // }
-
+    var img = product != null? product.getImage('large') : null;
+    var img_url = img!=null ? img.URL: null;
 
 
     res.render('home/home', {
         name: 'Start',
-        // product: {
-        //     url: product.getImage('large').URL
-        // }
+        product: {
+            name: product.getName(),
+            url: img_url
+        }
     })
     next();
 });
